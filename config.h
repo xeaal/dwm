f637eadfd7146e6a98d2290b217378bc5cf9a5e9
xeaal <tryax.use@gmail.com>
2023-03-30 08:29:03 +0000

    configuration slightly changed again

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
